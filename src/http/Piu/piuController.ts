48b8e981498a9fb2be8af8e42c6d4de41955f4f8
Iryoda <iryodap@gmail.com>
2020-12-07 05:17:09 -0300

    third-commit

--- src/http/Piu/piuController.ts
+++ src/http/Piu/piuController.ts
@@ -2,8 +2,8 @@ import {Response, Request} from 'express';
 
 
 export default class PiuController{
-    async index( request: Request. reponse: Response){
-        
+    async index( request: Request, reponse: Response){
+
     }
 
 }
